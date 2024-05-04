046e47ce556bb062bdc0a26526ce954f506d7633
Tim-den-Bode <330583@student.mboutrecht.nl>
2024-05-05 00:29:15 +0200

    playable basics
    I have added important features to the game, like the enemy being able to shoot, keeping track of player lives and scores, and allowing players to pause the game. The game can be played now, but other elements must be added for a better experience.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,11 +6,12 @@ public class Projectile : MonoBehaviour
 {
     public float moveSpeed;
     public GameObject explosionPrefab;
+    private PointManager pointManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pointManager = GameObject.Find("PointManager").GetComponent<PointManager>();
     }
 
     // Update is called once per frame
@@ -21,10 +22,27 @@ public class Projectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Enemy") 
+        if(collision.gameObject.tag == "Enemy 1") 
         { 
             Instantiate(explosionPrefab, transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
+            pointManager.UpdateScore(10);
+            Destroy(gameObject);
+        }
+
+        else if (collision.gameObject.tag == "Enemy 2")
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(collision.gameObject);
+            pointManager.UpdateScore(20);
+            Destroy(gameObject);
+        }
+
+        else if (collision.gameObject.tag == "Enemy 3")
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(collision.gameObject);
+            pointManager.UpdateScore(40);
             Destroy(gameObject);
         }
 
